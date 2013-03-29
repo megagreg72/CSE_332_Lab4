74ddfa812cbf6c4c3d86c896de23f499c9120d69
Daniel Goldberg <dag333@gmail.com>
2013-03-29 16:39:39 -0500

    Player files

---
+++ Player.cpp
@@ -0,0 +1,38 @@
+#include "stdafx.h"
+#include "Player.h"
+#include "Lab4.h"
+
+Player::Player(char * name){
+	_name = name;
+	_h = Hand();
+	won = 0;
+	lost = 0;
+
+	read_file(name, won, lost); //reads from file: name, wins, losses
+}
+
+
+void Player::read_file (char * in_file, unsigned int & won, unsigned int & lost){
+	vector<string> vs;
+	string txt = ".txt";
+	string in = in_file + txt;
+	ifstream ifs (in);
+	if( ifs ){//If input file cannot be opened/doesn't exist
+		string word;
+		while(ifs >> word){ //put input stream into vector of strings
+			vs.push_back(word);
+		}
+
+		if(vs[0] == in_file){ //file has structure: Name Wins Losses
+			won = stoi(vs[1]);
+			lost = stoi(vs[2]);
+		}
+	}
+
+}
+
+
+ostream& operator<<(ostream &out, const Player &p){
+	out << p._name << ": " << "Wins = " << p.won << ", Losses = " << p.lost;
+	return out;
+}
